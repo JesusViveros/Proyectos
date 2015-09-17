41bce7d83d1c6d39620166a92f728cb9d136e485
JesusViveros <gostrecon@live.com.mx>
2015-09-17 16:35:26 -0500

    Actualizado

--- Nao/Avanzar.py
+++ Nao/Avanzar.py
@@ -9,29 +9,31 @@ from naoqi import ALProxy
 
 def Girar(x):
     print "gira"
-    #Caminar(motionProxy,0.0,0.0,0.5*x,0.3)
-    #time.sleep(5)
-    #Caminar(motionProxy,0,0,0,0)
+    Caminar(motionProxy,0.0,0.0,0.45*x,0.3)
+    time.sleep(5)
+    Caminar(motionProxy,0,0,0,0)
 
 def Avanzar():
     print "Avanza"
-    #Caminar(motionProxy,0.8,0,0,0.2)
-    #time.sleep(6)
+    Caminar(motionProxy,0.8,0,0,0.2)
+    time.sleep(6)
 
 def Arrivar():
+    orientation='N'
     ant='n'
     m=0
     for line in open("2Dpoints"):
-    	print "                   FOR"
+        print "                   FOR"
         a=[int(i) for i in line[1:-2].split(",")]
         if ant=='n':
             print "ant=='n'"
             ant=a
             continue
-
         print "ant: "+str(ant)+" a: "+str(a)+" m: "+str(m)
+        band=False
         if a[m]==ant[m]:
             print "a[m]==ant[m]"
+            band=True
             if m==0:
                 m=1
                 print "M==1"
@@ -39,9 +41,19 @@ def Arrivar():
                 m=0
                 print "M==0"
 
-            if a[m]>ant[m]:
+        if a[m]>ant[m] and band==True:
+            print "a[m]>ant[m]"
+            if orientation=='N':
+                print "orientation=='N'"
+                orientation='D'
                 Girar(1)
-            else:
+            elif orientation=='D':
+                print "orientation=='D'"
+                orientation='S'
+                Girar(1)
+            elif orientation=='S':
+                print "orientation=='S'"
+                orientation=='D'
                 Girar(-1)
         ant=a
         Avanzar()
@@ -56,7 +68,6 @@ def main(robotIP,robotPort):
         motionProxy = ALProxy("ALMotion", robotIP, robotPort)
     except Exception, e:
         print "Could not create proxy to ALMotion"
-
     try:
         postureProxy = ALProxy("ALRobotPosture", robotIP, robotPort)
     except Exception, e:
@@ -77,7 +88,7 @@ def main(robotIP,robotPort):
     motionProxy.stiffnessInterpolation("Body",0,1.0)
 
 if __name__ == "__main__":
-    robotIp = "192.168.1.102"
+    robotIp = "192.168.1.104"
     robotPort = 9559
-    #main(robotIp,robotPort)
-    Arrivar()
+    main(robotIp,robotPort)
+    #Arrivar()
